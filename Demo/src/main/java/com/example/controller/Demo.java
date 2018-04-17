3b6c7db98b8dde1b55ff16e01be33aaa32ea6074
371122695@qq.com <371122695@qq.com>
2018-04-17 20:35:23 +0800

    swagger

--- Demo/src/main/java/com/example/controller/Demo.java
+++ Demo/src/main/java/com/example/controller/Demo.java
@@ -1,16 +1,15 @@
 package com.example.controller;
 
+import com.example.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@Api(value = "v1",description = "这是我的第一个版本的demo")
+@Api(value = "/",description = "这是我的第一个版本的demo")
 @RequestMapping("v1")
 public class Demo {
 
@@ -22,6 +21,25 @@ public class Demo {
     @ApiOperation(value = "获取用户数",httpMethod = "GET")
     public int getUserCount(){
         return template.selectOne("getUserCount");
+    }
+
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
+    @ApiOperation(value = "增加用户",httpMethod = "POST")
+    public int addUser(@RequestBody User user){
+        int result = template.insert("addUser",user);
+        return result;
+    }
+
+    @RequestMapping(value = "/updateUser",method = RequestMethod.POST)
+    @ApiOperation(value = "修改用户",httpMethod = "POST")
+    public int updateUser(@RequestBody User user){
+        return  template.update("updateUser",user);
+    }
+
+    @RequestMapping(value = "/deleteUser",method = RequestMethod.GET)
+    @ApiOperation(value = "删除用户",httpMethod = "GET")
+    public int delUser(@RequestParam int id){
+        return template.delete("deleteUser",id);
     }
 
 }
---
+++ Demo/src/main/java/com/example/model/User.java
@@ -0,0 +1,12 @@
+package com.example.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private int id;
+    private String name;
+    private String sex;
+    private int age;
+
+}
